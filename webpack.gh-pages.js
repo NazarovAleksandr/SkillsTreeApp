c57bdd85d7c199e284e6f2cb781666aa7d711f58
Nazarov Aleksandr <tryikkk@mail.ru>
2019-08-12 18:10:22 +0300

    Added webpack preset for gh-pages

---
+++ webpack.gh-pages.js
@@ -0,0 +1,17 @@
+const merge = require('webpack-merge');
+const common = require('./webpack.common.js');
+const webpack = require('webpack');
+
+module.exports = merge(common, {
+    mode: 'production',
+    plugins: [
+        new webpack.DefinePlugin({
+            'process.env': {
+                'BASENAME': JSON.stringify('SkillsTreeApp')
+            }
+        })
+    ],
+    output: {
+        publicPath: '/SkillsTreeApp/',
+    }
+});
--- webpack.prod.js
+++ webpack.prod.js
@@ -1,17 +1,6 @@
 const merge = require('webpack-merge');
 const common = require('./webpack.common.js');
-const webpack = require('webpack');
 
 module.exports = merge(common, {
-    mode: 'production',
-    plugins: [
-        new webpack.DefinePlugin({
-            'process.env': {
-                'BASENAME': JSON.stringify('SkillsTreeApp')
-            }
-        })
-    ],
-    output: {
-        publicPath: '/SkillsTreeApp/',
-    }
+    mode: 'production'
 });
